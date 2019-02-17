4ed1554e3d15581a12497a57d8c32ef91382dfb8
Brock Gibson <brockgibson@Brocks-MacBook-Pro.local>
2019-02-16 21:37:59 -0600

    First Commit

---
+++ BingeCapstoneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingeCapstoneProject
+//
+//  Created by Brock Gibson on 2/16/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
