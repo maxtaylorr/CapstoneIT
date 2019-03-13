621a3aecb103a4b44afb2f8b0c48f259401a80f6
Brock Gibson <brockgibson@mwc-019085.dhcp.missouri.edu>
2019-03-12 21:56:36 -0500

    Successful communication between app and realtime database

--- BingeCapstoneProject/AppDelegate.swift
+++ BingeCapstoneProject/AppDelegate.swift
@@ -14,10 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // firebase config
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        
+        let db = Database.database().reference()
+        db.setValue("Hello Firebase")
+        
         return true
     }
 
