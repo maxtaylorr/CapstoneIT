51d5a77f13e2d825cba77dd15bc247c677998a28
Brock Gibson <brockgibson@mwc-019149.dhcp.missouri.edu>
2019-03-14 10:10:46 -0500

    Added FireStone connectivity

--- BingeCapstoneProject/BarListViewController.swift
+++ BingeCapstoneProject/BarListViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class BarListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dbFire = Firestore.firestore().collection("bars")
+        dbFire.addDocument(data: ["Max": "Hey"])
 
         // Do any additional setup after loading the view.
     }
---
+++ BingeCapstoneProject/Bars.swift
@@ -0,0 +1,11 @@
+//
+//  Bars.swift
+//  BingeCapstoneProject
+//
+//  Created by Brock Gibson on 3/14/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import Foundation
+
+//Data Model for Bar Lists
