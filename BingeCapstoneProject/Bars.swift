2bbf801d3405634b076dd46d23bba64272d82cdd
Max Taylor <maxwell97taylor@gmail.com>
2019-04-02 09:50:13 -0500

    Data types updated

--- BingeCapstoneProject/Bars.swift
+++ BingeCapstoneProject/Bars.swift
@@ -17,10 +17,11 @@ class Bar {
     let latitude: String
     let longitude: String
     let openingTime: String
-    var deals = [String]()
+    var deals = [Deal]()
 
     init(name: String, date: Date, latitude: String, longitude: String, openingTime: String, deals: [String]) {
         self.name = name
+        self.date = date
         self.latitude = latitude
         self.longitude = longitude
         self.openingTime = openingTime
