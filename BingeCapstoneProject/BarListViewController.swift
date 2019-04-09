40b1c9b73ca8697515be21057a9cebb0a376e4aa
Jack Huffman <jack.huffman17@gmail.com>
2019-04-09 10:32:58 -0500

    Started table view

--- BingeCapstoneProject/BarListViewController.swift
+++ BingeCapstoneProject/BarListViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 
-class BarListViewController: UIViewController {
-
+class BarListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // table view
+    @IBOutlet weak var barTableView: UITableView!
+    
+    // bars
+    var bars = [Bar]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let dbFire = Firestore.firestore().collection("bars")
         dbFire.addDocument(data: ["Max": "Hey"])
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bars.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "barCell"
+        let bar = bars[indexPath.row]
+        
+        let cell = barTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        if let cell = cell as? BarTableViewCell {
+            cell.barTitleLabel.text = bar.name
+            cell.BarDescLabel.text = bar.openingTime
+        }
+        return cell
     }
     
 
