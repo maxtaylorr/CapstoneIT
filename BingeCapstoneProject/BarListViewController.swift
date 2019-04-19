532dcc24f0e8d58b7723e25de009f55a1c904118
maxtaylorr <max2smash1@gmail.com>
2019-04-18 21:00:39 -0500

    Manually adding back stuff
    Messed up adding new stuff to list view

--- BingeCapstoneProject/BarListViewController.swift
+++ BingeCapstoneProject/BarListViewController.swift
@@ -1,59 +1,56 @@
-//
-//  Bars.swift
+//  BarListViewController.swift
 //  BingeCapstoneProject
 //
-//  Created by Brock Gibson on 3/14/19.
+//  Created by Brock Gibson on 2/16/19.
 //  Copyright © 2019 Brock Gibson. All rights reserved.
 //
-
-import Foundation
 import UIKit
 import Firebase
 import FirebaseFirestore
-//Data Model for Bar Lists
+import Kingfisher
 
-var db: Firestore!
-
-
-class Bar {
+class BarListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let name: String
-    let date: Date
-    let latitude: Double
-    let longitude: Double
-    let openingTime: String
-    let imageURL: String
-    var deals = [Deal]()
+    var db: Firestore!
     
-    init(name: String, date: Date, latitude: Double, longitude: Double, openingTime: String, imageURL: String, deals: [Deal]) {
-        self.name = name
-        self.date = date
-        self.latitude = latitude
-        self.longitude = longitude
-        self.openingTime = openingTime
-        self.imageURL = imageURL
-        self.deals = deals
-    }
+    // table view iboutlet
+    @IBOutlet weak var barsTableView: UITableView!
     
-    class func fetchAllBars() -> [Bar] {
-        
-        var bars = [Bar]()
+    // bars
+    var bars = [Bar]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        barsTableView.dataSource = self
+        barsTableView.delegate = self
         
         let settings = FirestoreSettings()
         Firestore.firestore().settings = settings
         db = Firestore.firestore()
         
+        pullData()
+        DispatchQueue.main.async {
+            self.barsTableView.reloadData()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    func pullData() {
         db.collection("bars_04_02_2019").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 for document in querySnapshot!.documents {
-                    
+
                     let name = document.data()["name"] as! String
                     let imageURL = document.data()["imageURL"] as! String
                     let hours = document.data()["hoursOpen"] as! String
                     let deals = document.data()["deals"] as? Array ?? [""]
-                    
+
                     var lat: Double = 0.0
                     var lon: Double = 0.0
                     if let coords = document.get("coords") {
@@ -61,56 +58,69 @@ class Bar {
                         lat = point.latitude
                         lon = point.longitude
                     }
-                    
-                    let bar = Bar(name: name, date: Date(), latitude: lat, longitude: lon, openingTime: hours, imageURL: imageURL, deals: [])
-                    
-                    bars.append(bar)
-                    print(bar.name)
-                }
-            }
-        }
-        
-        return bars
-    }
-    
-}
 
-class Deal {
-    let hours: String
-    let deals: [String]
-    
-    init(hours: String, deals: [String]) {
-        self.hours = hours
-        self.deals = deals
-    }
-    
-    class func loadDeals(_ deals: [String]) -> [Deal]{
-        
-        var dealsArray: [Deal] = []
-        
-        let trigger = CharacterSet(charactersIn: "$")
-        var hours: String
-        var dealsStringArray: [String] = []
-        
-        for deal in deals {
-            if let test = deal.rangeOfCharacter(from: trigger) {
-                print("found money sign")
-                
-                dealsStringArray.append(deal)
-                
-            } else {
-                print("no money sign")
-                
-                hours = deal
-                
-                if dealsStringArray.count > 0 {
-                    dealsArray.append(Deal(hours: hours, deals: dealsStringArray))
+                    var dealsArray: [Deal] = []
+                    
+                    let trigger = CharacterSet(charactersIn: "$")
+                    var dealHours: String = ""
+                    var dealsStringArray: [String] = []
+                    
+                    for deal in deals {
+                        if let test = deal.rangeOfCharacter(from: trigger) {
+                            dealsStringArray.append(deal)
+                            
+                        } else {
+                            if dealsStringArray.count > 0 {
+                                dealsArray.append(Deal(hours: dealHours, deals: dealsStringArray))
+                                dealsStringArray = []
+                            }
+                            
+                            dealHours = deal
+                        }
+                    }
+                    dealsArray.append(Deal(hours: dealHours, deals: dealsStringArray))
                     dealsStringArray = []
-                    hours = ""
+                    
+                    let bar = Bar(name: name, date: Date(), latitude: lat, longitude: lon, openingTime: hours, imageURL: imageURL, deals: dealsArray)
+                    
+                    self.bars.append(bar)
                 }
+            }
+            DispatchQueue.main.async {
+                self.barsTableView.reloadData()
             }
         }
         
-        return dealsArray
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bars.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "barCell"
+        let bar = bars[indexPath.row]
+        let url = URL(string: bar.imageURL)
+        //let processor = RoundCornerImageProcessor(cornerRadius: 20)
+        //let processor = CroppingImageProcessor(size: CGSize(width: 100, height: 100), anchor: CGPoint(x: 0.5, y: 0.5))
+
+        let cell = barsTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        if let cell = cell as? BarTableViewCell {
+            cell.barTitleLabel.text = bar.name
+            cell.BarDescLabel.text = bar.openingTime
+            cell.barImage.kf.setImage(with: url)
+        }
+        return cell
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? BarDetailViewController, let selectedRow = barsTableView.indexPathForSelectedRow {
+            destination.passedBar = bars[selectedRow.row]
+        }
+    }
+
 }
